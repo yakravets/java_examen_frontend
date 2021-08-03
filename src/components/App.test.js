1b8bd3235049c974b3d9f8942cbb956e7b98f759
yakravets <30793298+yakravets@users.noreply.github.com>
2021-08-04 00:57:03 +0300

    update test/snapshot

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -16,23 +16,3 @@ it("renders correctly", () => {
     const tree = shallow(<App />);
     expect(toJson(tree)).toMatchSnapshot();
 });
-
-beforeEach(() => {
-        // Clear all instances and calls to constructor and all methods:
-        App.mockClear();
-    }
-);
-
-it('We can check if the consumer called a method on the class instance', () => {
-    // Show that mockClear() is working:
-    expect(App).not.toHaveBeenCalled();
-  
-    const testAppConsumer = new AppConsumer();
-    // Constructor should have been called again:
-    expect(App).toHaveBeenCalledTimes(1);
-});
-
-it('We can check if the consumer called the class constructor', () => {
-    const testAppConsumer = new AppConsumer();
-    expect(App).toHaveBeenCalledTimes(1);
-});
